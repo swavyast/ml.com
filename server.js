1cad4a73992bc0e53dadac67612fc7902085af4b
Himanshu Rai <swavyast@gmail.com>
2024-05-07 00:00:13 +0530

    ml.com feature commit:000000000000000000000000005 : updated server.js
    Signed-off-by: Himanshu Rai <swavyast@gmail.com>

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+
+
+var express = require('express');
+var cors = require('cors');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+var bodyParser = require('body-parser');
+
+const app = express();
 
 const REACT_APP_GITHUB_CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID;
 const REACT_APP_GITHUB_CLIENT_SECRET = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
 
-const app = express();
+console.log('client id and secret in server.js');
+console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
 
 app.use(cors());
 app.use(bodyParser.json());
 
-
-
-app.get('getAccessToken', async function (req, res) {
-    req.query.code;
+app.get('/getAccessToken', async function (req, res) {
+    console.log(req.query.code);
 
     const params = '?client_id=' + REACT_APP_GITHUB_CLIENT_ID + '&client_secret='
         + REACT_APP_GITHUB_CLIENT_SECRET + '&code=' + req.query.code;
 
-    await fetch('https://github.com/login/oauth/login/access_token=' + params, {
+    await fetch('https://github.com/login/oauth/access_token=' + params, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
@@ -33,6 +37,19 @@ app.get('getAccessToken', async function (req, res) {
 })
 
 
-
+app.get('/getUserData', async function (req, res){
+    req.get('Authorization');   //Bearer access-token
+    await fetch('https://api.github.com/user', {
+        method : 'GET',
+        headers : {
+            'Authorization' : req.get('Authorization')   //Bearer access-token
+        }
+    }).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        console.log('response of cors server : [userData] =>', data);
+        res.json(data);
+    })
+});
 
 app.listen(4000, () => console.log('CORS server is running on port : 4000'));
